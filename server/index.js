430e6310904eba65c3750612fc252bfd8b5d843e
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-24 14:34:57 -0700

    version 1.1 ----added rollbar setting and code

--- server/index.js
+++ server/index.js
@@ -13,3 +13,14 @@ app.get('/', (req,res) => {
 app.listen(port, () => {
     console.log(`Port running on ${port}`);
 })
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '9a235518dac540f789366037c5ff9d5d',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");

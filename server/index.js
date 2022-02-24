39e1c41c78eb78f313a37c489d86141a80fee153
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-24 14:30:11 -0700

    version 1 ---Server built without rollbar

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '..public/Home.html'));
+    res.sendFile(path.join(__dirname, '../public/Home.html'));
 })
 
 app.listen(port, () => {

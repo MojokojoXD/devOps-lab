6a31b78356340e31bc3c6e1cf0adfe1d19a2d77f
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-24 15:23:03 -0700

    version 1.2 test non existent function w/rollbar

--- public/home.js
+++ public/home.js
@@ -3,7 +3,7 @@ const button = document.createElement('button')
 button.textContent = 'CLick here';
 
 function errorTest(){
-    axios.get('https://mojokojo-devops-lab.herokuapp.com/test').then(() => {
+    axios.get('https://mojokojo-devops-lab.herokuapp.com/api/test').then(() => {
         console.log('it worked');
     }).catch(err => console.log(err))
 }

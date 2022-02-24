0541ac842e9b751e3a3db22907b9ce12441a3716
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-02-24 15:10:00 -0700

    version 1.1 ----added rollbar setting and code

---
+++ public/home.js
@@ -0,0 +1,14 @@
+const button = document.createElement('button')
+
+button.textContent = 'CLick here';
+
+function errorTest(){
+    axios.get('https://mojokojo-devops-lab.herokuapp.com/test').then(() => {
+        console.log('it worked');
+    }).catch(err => console.log(err))
+}
+
+
+button.addEventListener('click', errorTest)
+
+document.body.appendChild(button);
--- server/index.js
+++ server/index.js
@@ -10,17 +10,20 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/Home.html'));
 })
 
+app.use(express.static(path.join(__dirname, '../public')))
+app.get('/api/test', testEndpoint);
+
 app.listen(port, () => {
     console.log(`Port running on ${port}`);
 })
 
 // include and initialize the rollbar library with your access token
-var Rollbar = require("rollbar");
-var rollbar = new Rollbar({
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
   accessToken: '9a235518dac540f789366037c5ff9d5d',
   captureUncaught: true,
   captureUnhandledRejections: true
 });
 
 // record a generic message and send it to Rollbar
-rollbar.log("Hello world!");
+rollbar.log("DevOps Lab");
